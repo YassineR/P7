aa1b289708a8b9723a81fd1a625ad016a3c81474
YassineR <yassine.rachedi@yahoo.fr>
2023-09-05 18:01:12 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,12 +8,21 @@ import json
 import pickle
 from PIL import Image
 from lightgbm import LGBMClassifier
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+from matplotlib.colors import rgb2hex
+import plotly.graph_objs as go
 
 st.set_page_config(
     page_title="Dashboard Credit Score Client",
     layout="wide" 
 )
 
+def set_color_range(probability):
+    cmapR = cm.get_cmap('RdYlGn')
+    norm = Normalize(0, 1)
+    return rgb2hex(cmapR(norm(probability)))
+    
 
 @st.cache_resource 
 def load_model():
@@ -67,6 +76,22 @@ with col12:
     if(st.session_state.prediction == -1):
         st.session_state.text =  "Client introuvable "+ str(type(st.session_state.index)) + "  --  " + str(st.session_state.index)
         st.text(body = st.session_state.text  )
+
+    else:        
+        color = set_color_range(st.session_state.prediction)
+        fig = go.Figure(go.Indicator(
+            mode="gauge+number",
+            number={'suffix': "%"},
+            value= st.session_state.prediction,
+            domain={'x': [0, 1], 'y': [0, 1]},
+            title={'text': "Client sûr"},
+            gauge={'axis': {'range': [None, 100]},
+                   'bar' : {'color': color}
+                  },
+        ))
+        
+        st.plotly_chart(fig, use_container_width=True)
+
     
     st.number_input('EXT_SOURCE_3', key = 'ext_source_3',
                                  min_value=0., step=0.1, max_value = 1.)
