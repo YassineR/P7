d338bf9106dd49d7ebea97caef25c7a0757b4270
YassineR <yassine.rachedi@yahoo.fr>
2023-09-08 21:34:41 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -97,7 +97,7 @@ with col12:
         
         st.plotly_chart(gauge_fig, use_container_width=True)
         
-        col121, col122 = st.columns()
+        col121, col122 = st.columns(2)
         
         global_FI_fig = get_plot_global_feature_importance()
         local_FI_fig = get_plot_local_feature_importance(data.iloc[[0]])
