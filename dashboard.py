96b362df1147ab0ace23841952227bd83a9a1bca
YassineR <yassine.rachedi@yahoo.fr>
2023-09-08 21:40:23 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -99,8 +99,8 @@ with col12:
         
         col121, col122 = st.columns(2)
         
-        global_FI_fig = Tools.get_plot_global_feature_importance(lgbm,data.columns)
-        local_FI_fig = Tools.get_plot_local_feature_importance(data.iloc[[0]])
+        global_FI_fig = Tools.get_plot_global_feature_importance(lgbm,st.session_state.row.columns)
+        local_FI_fig = Tools.get_plot_local_feature_importance(st.session_state.row)
         
         with col121:
             st.plotly_chart(global_FI_fig, use_container_width=True)
